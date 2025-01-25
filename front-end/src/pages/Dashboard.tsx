fa4c9174af48cf415e1099a25074cd7c11bda239
Alex Arguelles <alexarguelles87923@gmail.com>
2025-01-24 23:06:40 -0500

    start settings

--- front-end/src/pages/Dashboard.tsx
+++ front-end/src/pages/Dashboard.tsx
@@ -16,7 +16,7 @@ interface chatUser {
 
 const dashboard: React.FC = () => {
 	const { userContext } = useContext(UserContext);
-	// const [hidePassVer, setHidePassVer] = useState(false);
+	// const [hideEmailVer, setHideEmailVer] = useState(false);
 
 	useEffect(() => {
 		console.log("Loaded dashboard page...");
@@ -66,7 +66,7 @@ const dashboard: React.FC = () => {
 
       return (
 		<>
-			{/* <div className={`relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1 ${hidePassVer ? 'hidden' : ''}`}>
+			{/* <div className={`relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1 ${hideEmailVer ? 'hidden' : ''}`}>
 				<div className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl" aria-hidden="true">
 					<div className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30" style={divStyle}></div>
 				</div>
@@ -80,7 +80,7 @@ const dashboard: React.FC = () => {
 					<a href="#" className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900">Send email<span aria-hidden="true">&rarr;</span></a>
 				</div>
 				<div className="flex flex-1 justify-end">
-					<button type="button" onClick={() => setHidePassVer(true)} className="-m-3 p-3 focus-visible:outline-offset-[-4px]">
+					<button type="button" onClick={() => setHideEmailVer(true)} className="-m-3 p-3 focus-visible:outline-offset-[-4px]">
 					<span className="sr-only">Dismiss</span>
 					<svg className="size-5 text-gray-900" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
 						<path d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z" />
